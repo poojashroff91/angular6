db55629d69819b1f1389daa437ce91d8cbac4569
Shroff, Pooja <poshroff@paypal.com>
2018-06-11 13:58:00 +0530

    Alternate way of adding input properties

--- hello-world/src/app/favorite/favorite.component.ts
+++ hello-world/src/app/favorite/favorite.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  inputs: ['isFavorite']
 })
 export class FavoriteComponent implements OnInit {
-  @Input() isFavorite: boolean;
+  isFavorite: boolean;
 
   onClick(){
     this.isFavorite = !this.isFavorite;
