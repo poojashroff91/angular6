a43c5d7eff2f5c2caf82316b9eaeb408e54ea93c
Shroff, Pooja <poshroff@paypal.com>
2018-06-12 00:30:27 +0530

    ngClass

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -25,17 +25,25 @@ export class AppComponent {
   // courses = [1, 2];
   viewMode="map";
 
-  courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'}
-  ];
+  courses;
   onAdd(){
     this.courses.push({id: 4, name: 'course4'});
   }
   onRemove(course){
     let index = this.courses.indexOf(course);
     this.courses.splice(index, 1);
+  }
+
+  loadCourses(){
+    this.courses = [
+      { id: 1, name: 'course1'},
+      { id: 2, name: 'course2'},
+      { id: 3, name: 'course3'}
+    ];
+  }
+
+  trackCourse(index, course){
+    return course ? course.id: undefined;
   }
 
 }
