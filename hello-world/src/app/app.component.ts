2670e01b16e43a4147e39722f531b8e7c41c43e2
Shroff, Pooja <poshroff@paypal.com>
2018-06-11 22:01:41 +0530

    ngFor and change detection

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -25,4 +25,17 @@ export class AppComponent {
   // courses = [1, 2];
   viewMode="map";
 
+  courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'}
+  ];
+  onAdd(){
+    this.courses.push({id: 4, name: 'course4'});
+  }
+  onRemove(course){
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+
 }
