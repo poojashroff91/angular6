719ff6412b7c5e46336463827ddf97401b5c7fed
Shroff, Pooja <poshroff@paypal.com>
2018-06-18 13:30:47 +0530

    Validators

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -40,9 +40,9 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
     SignupFormComponent
   ],
   imports: [
-  
-BrowserModule,
-  FormsModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     CoursesService,
