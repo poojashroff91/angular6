4fb5972ff6476e31feced6f1fb89fb87fe4718be
Shroff, Pooja <poshroff@paypal.com>
2018-06-18 16:01:07 +0530

    Validations continued

--- hello-world/src/app/signup-form/signup-form.component.ts
+++ hello-world/src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
 
 @Component({
   selector: 'signup-form',
@@ -8,11 +9,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', Validators.required),
+    username: new FormControl('', [
+        Validators.required, 
+        Validators.minLength(3),
+        UsernameValidators.cannotContainSpace
+      ], 
+      UsernameValidators.shouldBeUnique
+     ),
     password: new FormControl('', Validators.required)
   });
 
   get username() {
     return this.form.get('username');
+  }
+
+  login() {
+    /*this.form.setErrors({
+      invalidLogin: true
+    });*/
+
   }
 }
