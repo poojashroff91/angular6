17d3b0116e642af26eec0a1792038b767150a21c
Shroff, Pooja <poshroff@paypal.com>
2018-06-20 23:03:17 +0530

    Services added

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -1,39 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
+import {Component, OnInit} from '@angular/core';
+import {PostService} from '../services/post.service';
 
-@Component({
-  selector: 'posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
-})
+@Component({selector: 'posts', templateUrl: './posts.component.html', styleUrls: ['./posts.component.css']})
 export class PostsComponent implements OnInit {
 
-  posts: any[];
-  private url = 'https://jsonplaceholder.typicode.com/posts';
+  posts : any[];
 
-  constructor(private http: Http) { 
-    http.get(this.url)
-    .subscribe(response =>{
-      this.posts = response.json();
-    });
-  }
+  constructor(private service : PostService) {}
 
-  createPost(input: HTMLInputElement) {
+  createPost(input : HTMLInputElement) {
     let post = {
       title: input.value
     };
     input.value = '';
-    this.http.post(this.url, JSON.stringify(post))
+    this
+      .service
+      .createPost(post)
       .subscribe(response => {
-        post['id'] = response.json().id;
-        this.posts.splice(0, 0, post);
+        post['id'] = response
+          .json()
+          .id;
+        this
+          .posts
+          .splice(0, 0, post);
         console.log(response.json());
       });
   }
 
-  updatePost(post){
-    
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true}) )
+  updatePost(post) {
+
+    this
+      .service
+      .updatePost(post)
       .subscribe(response => {
         console.log(response);
       });
@@ -42,15 +40,28 @@ export class PostsComponent implements OnInit {
 
   deletePost(post) {
 
-    this.http.delete(this.url + '/' + post.id)
-      .subscribe( response => {
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
+    this
+      .service
+      .deletePost(post)
+      .subscribe(response => {
+        let index = this
+          .posts
+          .indexOf(post);
+        this
+          .posts
+          .splice(index, 1);
       });
 
   }
 
   ngOnInit() {
+    this
+      .service
+      .getPosts()
+      .subscribe(response => {
+        this.posts = response.json();
+      });
+
   }
 
 }
