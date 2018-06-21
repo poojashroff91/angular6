ae92b4780cc8986e95e68cd09c2e3d1f3b61b129
Shroff, Pooja <poshroff@paypal.com>
2018-06-21 11:17:03 +0530

    Resuable data service

--- hello-world/src/app/services/post.service.ts
+++ hello-world/src/app/services/post.service.ts
@@ -1,56 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
-import {AppError} from '../common/app-error';
-import {NotFoundError} from '../common/not-found-error';
-import {BadInput} from '../common/bad-input';
-
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
-
+import { DataService } from './data.service';
 @Injectable({providedIn: 'root'})
-export class PostService {
-  private url = 'https://jsonplaceholder.typicode.com/posts';
+export class PostService extends DataService {
 
-  constructor(private http : Http) {}
-
-  getPosts() {
-    return this
-      .http
-      .get(this.url)
-      .catch(this.handleError);
-  }
-
-  createPost(post) {
-    return this
-      .http
-      .post(this.url, JSON.stringify(post))
-      .catch(this.handleError);
-  }
-
-  updatePost(post) {
-    return this
-      .http
-      .patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
-      .catch(this.handleError);
-  }
-
-  deletePost(post) {
-    return this
-      .http
-      .delete(this.url + '/' + post.id)
-      .catch(this.handleError);
-  }
-
-  private handleError(error : Response) {
-
-    if (error.status === 404) 
-      return Observable.throw(new NotFoundError());
-    
-    if (error.status === 400) {
-      return Observable.throw(new BadInput(error.json()));
-    }
-    return Observable.throw(new AppError(error));
-
+  constructor(http : Http) {
+    super ('https://jsonplaceholder.typicode.com/posts', http);
   }
 }
